c48673262c4d01061ea9fb260ac69f8b6b763963
AyeshaAbdulGhaffar0900 <ayesha.a.ghaffar07@gmail.com>
2024-12-15 12:38:10 +0500

    React class 01

---
+++ React js/class 01/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <div>
+        <h1>React class 01</h1>
+        <h2>hello world</h2>
+    </div>
+);
